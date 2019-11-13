4fbea83187caa2e7ecb8927eab772e8090fee84e
Andre Costa <andrecostaa@gmail.com>
2019-11-13 16:52:46 -0300

    criacao das telas de listar e cadastrar categorias.

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,31 @@
 const express = require("express")
 const router = express.Router()
-
-router.get("/categories", (req, res) => {
-    res.send("ROTA DE CATEGORIAS")
-})
+const Category = require("./Category")
+const slugify = require("slugify")
 
 router.get("/admin/categories/new", (req, res) => {
-    res.send("ROTA PARA CRIAR NOVAS CATEGORIAS")
+    res.render("admin/categories/new")
+})
+
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title
+    if(title != undefined) {
+        Category.create({
+            title: title,
+            slug: slugify(title) // funcao, resultado exemplo: Computação e Informática => computacao-e-informatica
+        }).then(()=> {
+            res.redirect("/")
+        })
+    } else {
+        res.redirect("admin/categories/new")
+    }
+})
+
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", {categories: categories})
+    })
+    
 })
 
 module.exports = router;
